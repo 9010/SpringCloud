eb62abadabc7d9c7a12038c3ebb25c1fa2498d45
Prime <30680967+9010@users.noreply.github.com>
2019-04-09 14:55:40 +0800

    First commit

---
+++ src/main/java/cn/com/self/ZX/ZxApplication.java
@@ -0,0 +1,12 @@
+package cn.com.self.ZX;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZxApplication.class, args);
+	}
+}
---
+++ src/main/java/cn/com/self/ZX/web/HelloController.java
@@ -0,0 +1,12 @@
+package cn.com.self.ZX.web;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @RequestMapping(value = "hello")
+    public String index(){
+        return "Hello world";
+    }
+}
