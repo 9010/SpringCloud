fd2de187dcd307fbd0755d4eadfbec63ffaf0cea
Prime <30680967+9010@users.noreply.github.com>
2019-04-09 16:35:23 +0800

    indicator test

--- src/main/java/cn/com/self/ZX/web/HelloController.java
+++ src/main/java/cn/com/self/ZX/web/HelloController.java
@@ -3,6 +3,10 @@ package cn.com.self.ZX.web;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * RESTful API
+ */
+
 @RestController
 public class HelloController {
     @RequestMapping(value = "hello")
